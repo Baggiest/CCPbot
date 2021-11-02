8e8a964150cce87a2eece47e22af97414d18c1d8
Qusai0 <qussay.me.abbas@gmail.com>
2021-11-03 00:23:38 +0300

    scuffed ass shit

--- commands/balance.js
+++ commands/balance.js
@@ -1,15 +1,10 @@
-const { MessageActionRow } = require("discord.js");
-const { Db } = require("mongodb");
-
 module.exports = {
     name: 'balance',
     description: 'prints your social balance',
     cooldown: 5,
-    isMod: true,
-    async execute(message, args,client,) {
-      const user = await message.client.dbInstance.collection('users').findOne({ id: message.author.id });
-      user.balance;
-      const m = await message.channel.send(user.balance.toString());
+    async execute(message, args,) {
+      const users = await message.client.dbInstance.collection('users').findOne({ id: message.author.id });  
+      const m = await message.channel.send(users.balance.toString())
     }
 
 };
