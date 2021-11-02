2f3a903a5ba17bebe1d597b2c3fb257fc196ce4c
DaCurse <dacurse0@gmail.com>
2021-11-02 19:04:10 +0200

    Start of huge refactor, added linting and formatting

--- commands/dbtest.js
+++ commands/dbtest.js
@@ -1,9 +1,11 @@
 module.exports = {
-    name: 'dbtest',
-    description: 'test!',
-    cooldown: 5,
-    async execute(message, args) {
-        const items = await message.client.dbInstance.collection('config').findOne({ ping: "pong" });
-        message.reply(items.ping)
-    },
+  name: 'dbtest',
+  description: 'test!',
+  cooldown: 5,
+  async execute(message) {
+    const items = await message.client.dbInstance
+      .collection('config')
+      .findOne({ ping: 'pong' });
+    message.reply(items.ping);
+  },
 };
--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,10 @@
 module.exports = {
-    name: 'say',
-    description: 'says what you input!',
-    cooldown: 5,
-    isMod: true,
-    async execute(message, args){
-        message.channel.send(args.join(" "))
-        message.delete()
-}};
+  name: 'say',
+  description: 'says what you input!',
+  cooldown: 5,
+  isMod: true,
+  async execute(message, args) {
+    message.channel.send(args.join(' '));
+    message.delete();
+  },
+};
