20fc136a4571cf29cf3489fcad3e84bdeb0d92cf
Qusai0 <qussay.me.abbas@gmail.com>
2021-11-02 22:28:59 +0300

    Revert "Merge https://github.com/MrBaggieBug/CCPbot into Qusai"
    This reverts commit 988efdfe3fbe70fe5964d106b3cc1a2492c43d95, reversing
    changes made to 1b842e95c90eafd9debe974fee0098b4141609a0.

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'say',
     description: 'says what you input!',
     cooldown: 5,
-    isMod: true,
     async execute(message, args){
         message.channel.send(args.join(" "))
         message.delete()
