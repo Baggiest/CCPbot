f5178504fc185cc344abfa7f5dcc66bd5fc48afb
Kaylon Rogers <91156297+KaylonR@users.noreply.github.com>
2021-11-02 17:13:30 +0000

    bruh

--- index.js
+++ index.js
@@ -25,15 +25,20 @@ async function logData(message){
 
 async function exeCommand(command, message, args) {
     await command.execute(message, args);
-}
-client.once('ready', async () => {
-    const databaseClient = await new MongoClient(config.databaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
+}    
+async function databaseConnect(){
+    databaseClient = await new MongoClient(config.databaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
     databaseClient.connect(err => {
         client.dbInstance = databaseClient.db(config.databaseName);
-    });
-
+        client.login(token)
+});
+}
+databaseConnect()
+client.once('ready', async () => {
     console.log("bot started")
 });
+const dupe = async()=> {client.dbInstance.collection("users").findMultiple({ uuid: 904739487580557362})}
+client.dbInstance.collection.deleteMany(dupe)
 let replies = { //autoreply system based on keywords
 };
 client.on("messageCreate", async message => {
