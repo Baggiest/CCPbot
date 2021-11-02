5157c0e65e8fef3c36371514e397537888801a23
Mani Sohi <Mani.ehsanisohi@gmail.com>
2021-11-02 19:56:29 +0330

    Merge branch 'kaylon' of https://github.com/MrBaggieBug/CCPbot into kaylon

--- commands/ourlordandsaviour.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ourlordandsaviour',
-    description: 'pong!',
-    cooldown: 5,
-    async execute(message, args) {
-        const m = await message.channel.send({ files: ["./files/lord.jpg"]});
-    },
-};
