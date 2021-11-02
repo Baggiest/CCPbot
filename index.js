21bd68b6ce29301c39cbf5cb89c7b9e4d5129009
Kaylon Rogers <91156297+KaylonR@users.noreply.github.com>
2021-11-02 19:13:19 +0000

    undone dacurse changes Clueless

--- index.js
+++ index.js
@@ -4,51 +4,53 @@ const config = require('./config.json');
 const fs = require('fs');
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
+const MongoClient = require('mongodb').MongoClient;
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 client.prefix = config.prefix;
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
+async function logData(message){
+    const user = await client.dbInstance.collection("users").findOne({ uuid: message.author.id})
+    if (user == null){
+        const china = { uuid: message.author.id, balance: 1000}
+        client.dbInstance.collection("users").insertOne(china);
+        console.log("entry made")
+        }
+    else{
+    
+    }
+}
 
 async function exeCommand(command, message, args) {
     await command.execute(message, args);
+}    
+async function databaseConnect(){
+    databaseClient = await new MongoClient(config.databaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    databaseClient.connect(err => {
+        client.dbInstance = databaseClient.db(config.databaseName);
+        client.login(token)
+}); 
 }
-
+databaseConnect()
 client.once('ready', async () => {
-    console.log("Bot Initialised")
+    console.log("bot started")
 });
-let replies = {
-    "shryder": "fucker",
-    "jebait": "https://cdn.discordapp.com/attachments/902931009283162153/904769760175198278/Superman.mp4",
-    "🍦": "BING CHILLING",
-    "dance": "https://cdn.discordapp.com/attachments/768262165525168168/904823066473926706/dance.mov"
+const dupe = async()=> {client.dbInstance.collection("users").findMultiple({ uuid: 904739487580557362})}
+client.dbInstance.collection("users").deleteOne(dupe)
+let replies = { //autoreply system based on keywords
 };
 client.on("messageCreate", async message => {
+    logData(message)
     if (message.content in replies) {
-        message.reply(replies[message.content]);
+        message.reply(replies[message.content]); //seperate client.on for let replies
         return;
     }
 })
 
 client.on('messageCreate', async message => {
-    if (message.content.toLowerCase().includes("nothing happened in tiananmen square")){
-        message.channel.send("+999 Social Credit")
-    }
-    if (message.content.toLowerCase().includes("winnie the pooh")){
-        message.channel.send("-100 Social Credit")
-    }
-    if (message.content.toLowerCase().includes("taiwan isn't real")){
-        message.channel.send("+100 Social Credit")
-    }
-    if (message.content.toLowerCase().includes("taiwan isnt real")){ //HOW THE FUCK DO I ADD THIS TO THE IF STATEMENT ABOVE JIM JIM HELP PLS TY
-        message.channel.send("+100 Social Credit")
-    if (message.content.toLowerCase().includes("taiwan is real")){
-        message.channel,send("-100 Social Credit")
-    }
-    }
     if (!(message.content.startsWith(client.prefix) || message.mentions.users.first() == client.user) || message.author.bot) return;
-    
     if (message.content.startsWith(client.prefix)) {
         args = message.content.slice(client.prefix.length).split(/ +/);
     } else {
