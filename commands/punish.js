e32574dbc583a84b3ca553d28f6acafbe8abe1b8
Kacper <stasikkacper1@gmail.com>
2021-11-02 22:40:54 -0500

    added punish

---
+++ commands/punish.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "punish",
+    description: "Removes balance from user",
+    cooldown: 5,
+    isMod: true,
+    async execute(message, args) {
+        const amount = args[1];
+        const punishedUser = message.mentions.users.first();
+        await message.client.dbInstance.collection('users').updateOne({ uuid: punishedUser.id }, {$inc: { balance: -(amount)}}); 
+        message.reply(`${punishedUser}'s balance has been deducted by ${amount} points.（｀Δ´）！`);
+    },
+    
+};
