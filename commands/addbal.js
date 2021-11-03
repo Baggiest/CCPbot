66be4c840ee3f552429b52f09dc5e398549ce130
Kaylon Rogers <91156297+KaylonR@users.noreply.github.com>
2021-11-03 21:41:10 +0000

    addbal/delbal cmds fixed - credit algo next - fuck u kacper

--- commands/addbal.js
+++ commands/addbal.js
@@ -1,20 +1,27 @@
 const { SystemChannelFlags } = require("discord.js")
 
 module.exports = {
-    name: 'addbal',
+    name: 'delbal',
     description: 'gets social credit balance',
     cooldown: 5,
     async execute(message, args) {
         const mention = message.mentions?.users?.first();
         const amount = parseInt(mention ? args[1] : args[0]);
-        const user = mention ? mention.id : message.author.id;
-        const userU = await message.client.dbInstance.collection('users').updateOne(
-            { uuid: user.id },
+        if(isNaN(amount)){
+            console.log("error parsing NaN");
+        } else {
+            const userid = mention ? mention.id : message.author.id;
+            const userB_OLD = await message.client.dbInstance.collection('users').findOne({ uuid: userid });
+            const userU = await message.client.dbInstance.collection('users').updateOne(
+                { uuid: userid },
+                {
+                    $inc: {balance: amount}
+                }
+            )
+            const userB = await message.client.dbInstance.collection('users').findOne({ uuid: userid });
+            message.reply(`Balance depleted to: ${userB.balance} from ${userB_OLD.balance}`);
             {
-                $inc: {balance: amount}
-            }
-        )
-        const userB = await message.client.dbInstance.collection('users').findOne({ uuid: user.id });
-        message.reply(user)
+    }
+        }
     }
 }
