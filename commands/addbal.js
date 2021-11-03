76973fe6bbfbdf3916d8493d7ef64d396f629a25
Kaylon Rogers <91156297+KaylonR@users.noreply.github.com>
2021-11-03 21:04:39 +0000

    help daddy dacurse

--- commands/addbal.js
+++ commands/addbal.js
@@ -5,30 +5,16 @@ module.exports = {
     description: 'gets social credit balance',
     cooldown: 5,
     async execute(message, args) {
-        mentionedUser = message.mentions.users.first()
-        let userObj
-        if (mentionedUser != null){
-            userObj = mentionedUser.id
-        } else {
-            userObj = message.author.id
-        }
-        console.log("Debugging.")
-        console.log(userObj)
-        console.log(args[0])
-        if (args[0] = int){
-            const amount = parseInt(args[0], 10)
-        } else{
-            const amount = parseInt(args[1], 10)
-        }
-        console.log(amount)
-        const user = await message.client.dbInstance.collection('users').updateOne(
-            { uuid: userObj },
+        const mention = message.mentions?.users?.first();
+        const amount = parseInt(mention ? args[1] : args[0]);
+        const user = mention ? mention.id : message.author.id;
+        const userU = await message.client.dbInstance.collection('users').updateOne(
+            { uuid: user.id },
             {
                 $inc: {balance: amount}
             }
         )
-        const userB = await message.client.dbInstance.collection('users').findOne({ uuid: userObj });
-        message.reply(userB.balance.toString());
-        
+        const userB = await message.client.dbInstance.collection('users').findOne({ uuid: user.id });
+        message.reply(user)
     }
 }
