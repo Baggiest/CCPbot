88f76170069c3170b0169e83b9820e68f6668cad
Qusai0 <qussay.me.abbas@gmail.com>
2021-11-04 21:46:02 +0300

    .

--- commands/addbal.js
+++ commands/addbal.js
@@ -1,26 +1,14 @@
 module.exports = {
-    name: 'delbal',
-    description: 'gets social credit balance',
+    name: 'add',
+    description: 'prints your social balance',
     cooldown: 5,
-    async execute(message, args) {
-        const mention = message.mentions?.users?.first();
-        const amount = parseInt(mention ? args[1] : args[0]);
-        if(isNaN(amount)){
-            console.log("error parsing NaN");
-        } else {
-            const userid = mention ? mention.id : message.author.id;
-            const userB_OLD = await message.client.dbInstance.collection('users').findOne({ uuid: userid });
-            const userU = await message.client.dbInstance.collection('users').updateOne(
-                { uuid: userid },
-                {
-                    $inc: {balance: amount}
-                }
-            )
-            const userB = await message.client.dbInstance.collection('users').findOne({ uuid: userid });
-            message.reply(`Balance depleted to: ${userB.balance} from ${userB_OLD.balance}`);
-            {
-    }
-        }
+    async execute(message, args,) {
+        if(message.author.id !== "266275283177308161") return;
+        const amount = parseInt(args[0])
+      const users = await message.client.dbInstance.collection('users').UpdateOne({ uuid: message.author.id },  {$inc: { balance : amount } });  
+      console.log(users)
+      const m = await message.reply('New balance'+ users.balance.toString())
+      
     }
 
 }
