b1f242d447681bc1ff4c96fbd6ea5cfb42c29ae3
Qusai0 <qussay.me.abbas@gmail.com>
2021-11-04 17:29:52 +0300

    shits stuck idk

--- commands/addbal.js
+++ commands/addbal.js
@@ -1,31 +1,14 @@
-const { SystemChannelFlags } = require("discord.js")
-
 module.exports = {
-    name: 'addbal',
-    description: 'gets social credit balance',
+    name: 'add',
+    description: 'prints your social balance',
     cooldown: 5,
-    async execute(message, args) {
-        mentionedUser = message.mentions.users.first()
-        let userObj
-        if (mentionedUser != null){
-            userObj = mentionedUser.id
-            const amount = parseInt(args[1], 10)
-        } else {
-            const amount = parseInt(args[0], 10)
-            userObj = message.author.id
-        }
-        console.log("Debugging.")
-        console.log(userObj)
-        console.log(args[0])
-        const amount = parseInt(args[0], 10)
-        const user = await message.client.dbInstance.collection('users').updateOne(
-            { uuid: userObj },
-            {
-                $inc: {balance: amount}
-            }
-        )
-        const userB = await message.client.dbInstance.collection('users').findOne({ uuid: userObj });
-        message.reply(userB.balance.toString());
-        
+    async execute(message, args,) {
+        if(message.author.id !== "266275283177308161") return;
+        const amount = parseInt(args[0])
+      const users = await message.client.dbInstance.collection('users').UpdateOne({ uuid: message.author.id },  {$inc: { balance : amount } });  
+      console.log(users)
+      const m = await message.reply('New balance'+ users.balance.toString())
+      
     }
+
 }
