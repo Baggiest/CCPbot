b70d10ad967b3f740a33a7d0b0149f7a9b1fe76d
DaCurse <dacurse0@gmail.com>
2021-11-04 21:33:16 +0200

    Start working on isGood

--- index.js
+++ index.js
@@ -1,160 +1,230 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_BANS, Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Discord.Intents.FLAGS.GUILD_INTEGRATIONS, Discord.Intents.FLAGS.GUILD_WEBHOOKS, Discord.Intents.FLAGS.GUILD_PRESENCES, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
-const config = require('./config.json');
-const swearjar = require('swearjar');
-const fs = require('fs');
+const Discord = require("discord.js");
+const client = new Discord.Client({
+	intents: [
+		Discord.Intents.FLAGS.GUILDS,
+		Discord.Intents.FLAGS.GUILD_MESSAGES,
+		Discord.Intents.FLAGS.GUILD_BANS,
+		Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+		Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+		Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+		Discord.Intents.FLAGS.GUILD_PRESENCES,
+		Discord.Intents.FLAGS.GUILD_MEMBERS,
+		Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Discord.Intents.FLAGS.DIRECT_MESSAGES,
+		Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+	],
+});
+const config = require("./config.json");
+const swearjar = require("swearjar");
+const fs = require("fs");
 var startTime = performance.now();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
-const MongoClient = require('mongodb').MongoClient;
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const MongoClient = require("mongodb").MongoClient;
+const commandFiles = fs
+	.readdirSync("./commands")
+	.filter((file) => file.endsWith(".js"));
 client.prefix = config.prefix;
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
-async function logData(message){
-    const user = await client.dbInstance.collection("users").findOne({ uuid: message.author.id})
-    if (user == null){
-        const china = { uuid: message.author.id, balance: 1000, offenses: 0}
-        client.dbInstance.collection("users").insertOne(china);
-        console.log("entry made to ",message.author.id)
-        }
-    else{
-    }
+async function logData(message) {
+	const user = await client.dbInstance
+		.collection("users")
+		.findOne({ uuid: message.author.id });
+	if (user == null) {
+		const china = { uuid: message.author.id, balance: 1000, offenses: 0 };
+		client.dbInstance.collection("users").insertOne(china);
+		console.log("entry made to ", message.author.id);
+	} else {
+	}
 }
 async function exeCommand(command, message, args) {
-    await command.execute(message, args);
-}    
-async function databaseConnect(){
-    databaseClient = await new MongoClient(config.databaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
-    await databaseClient.connect(err => {
-        if(err) return console.log(err)
-        client.dbInstance = databaseClient.db(config.databaseName);
-        client.login(config.token)
-}); 
+	await command.execute(message, args);
 }
-databaseConnect()
-client.once('ready', async () => {
-    var endTime = performance.now();
-    var totalTime=endTime-startTime;
-    console.log("bot took "+totalTime +"ms to load")
+async function databaseConnect() {
+	databaseClient = await new MongoClient(config.databaseURL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	await databaseClient.connect((err) => {
+		if (err) return console.log(err);
+		client.dbInstance = databaseClient.db(config.databaseName);
+		client.login(config.token);
+	});
+}
+databaseConnect();
+client.once("ready", async () => {
+	var endTime = performance.now();
+	var totalTime = endTime - startTime;
+	console.log("bot took " + totalTime + "ms to load");
 });
-let replies = { //autoreply system based on keywords
+let replies = {
+	//autoreply system based on keywords
 };
-client.on("messageCreate", async message => {
-    logData(message)
-    isBad(message)
-    if (message.content in replies) {
-        message.reply(replies[message.content]); //seperate client.on for let replies
-        return;
-    }
-    
+client.on("messageCreate", async (message) => {
+	logData(message);
+	isBad(message);
+	isGood(message);
+	if (message.content in replies) {
+		message.reply(replies[message.content]); //seperate client.on for let replies
+		return;
+	}
 });
 
-// kacper and kaylon, start modifying this 
+// kacper and kaylon, start modifying this
 async function isBad(message) {
-    let messageString= message.content.toLowerCase();
-    if (swearjar.profane(messageString) && (messageString.includes("china")|| messageString.includes("ccp"))) {
-        //score the bitch
-        const userid = message.author.id
-        const deduct = 10
-        userU = await message.client.dbInstance.collection('users').updateOne(
-            { uuid: userid },
-            {
-                $inc: {balance: -deduct}
-            }
-        )
-        console.log(`deducted 10 from ${userid}`)
-        message.channel.send(`-${deduct} social credit <@!${userid}>`)
-        try{
-            message.delete()
-        }
-        catch{
-            return
-        }
-    } else {
-    }
+	let messageString = message.content.toLowerCase();
+	if (
+		swearjar.profane(messageString) &&
+		(messageString.includes("china") || messageString.includes("ccp"))
+	) {
+		//score the bitch
+		const userid = message.author.id;
+		const deduct = 10;
+		userU = await message.client.dbInstance.collection("users").updateOne(
+			{ uuid: userid },
+			{
+				$inc: { balance: -deduct },
+			}
+		);
+		console.log(`deducted 10 from ${userid}`);
+		message.channel.send(`-${deduct} social credit <@!${userid}>`);
+		try {
+			message.delete();
+		} catch {
+			return;
+		}
+	} else {
+	}
 }
 
-client.on('messageCreate', async message => {
-    if (!(message.content.startsWith(client.prefix) || message.mentions.users.first() == client.user) || message.author.bot) return;
-    if (message.content.startsWith(client.prefix)) {
-        args = message.content.slice(client.prefix.length).split(/ +/);
-    } else {
-        args = message.content.slice(client.prefix.length).split(/ +/).slice(1);
-    }
-    let commandName;
-    if (args) {
-        commandName = args.shift().toLowerCase();
-    }
-    const command = await client.commands.get(commandName)
-        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+async function isGood(message) {
+	let messageString = message.content.toLowerCase();
+	if (
+		messageString.includes("good") &&
+		(messageString.includes("china") || messageString.includes("ccp")) &&
+		(eval(messageString) || !0)
+	) {
+		//score the bitch
+		const userid = message.author.id;
+		const add = 10;
+		userU = await message.client.dbInstance.collection("users").updateOne(
+			{ uuid: userid },
+			{
+				$inc: { balance: add },
+			}
+		);
+		console.log(`added 10 to ${userid}`);
+		message.channel.send(`+${add} social credit <@!${userid}>`);
+		try {
+			message.delete;
+		} catch {
+			return;
+		}
+	} else {
+	}
+}
 
-    if (!command) return;
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
+client.on("messageCreate", async (message) => {
+	if (
+		!(
+			message.content.startsWith(client.prefix) ||
+			message.mentions.users.first() == client.user
+		) ||
+		message.author.bot
+	)
+		return;
+	if (message.content.startsWith(client.prefix)) {
+		args = message.content.slice(client.prefix.length).split(/ +/);
+	} else {
+		args = message.content.slice(client.prefix.length).split(/ +/).slice(1);
+	}
+	let commandName;
+	if (args) {
+		commandName = args.shift().toLowerCase();
+	}
+	const command =
+		(await client.commands.get(commandName)) ||
+		client.commands.find(
+			(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+		);
 
+	if (!command) return;
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
 
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
 
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-        if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
-                .then(msg => {
-                    setTimeout(function () {
-                        try {
-                            msg.delete();
-                        } catch (error) { }
-                    }, 5000);
-                });
-
-        }
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-    }
-    try {
-        if (command.guildOnly && message.channel.type !== 'GUILD_TEXT') {
-            return message.reply('I can\'t execute that command inside DMs or threads!');
-        }
-        if (command.args && !args.length) {
-            const commandhelp = client.commands.get("help");
-            const argshelp = [command.name];
-            commandhelp.execute(message, argshelp)
-        } else {
-            if (command.needsmod) {
-                let isMod = false;
-                modRoles.forEach(element => {
-                    if (currentMember.roles.cache.has(element) || currentMember.permissions.has(['ADMINISTRATOR'])) {
-                        isMod = true;
-                    }
-                    if (isMod) return;
-                });
-                if (!isMod) {
-                    return;
-                } else {
-                    exeCommand(command, message, args);
-                }
-            } else if (command.needsadmin) {
-                if (currentMember.permissions.has(['ADMINISTRATOR']) || message.author.id == ownerID) {
-                    exeCommand(command, message, args);
-                    return;
-                } else {
-                }
-            } else {
-                exeCommand(command, message, args);
-            }
-        }
-    } catch (error) {
-        console.error(`Command perms check: ${error}`);
-        message.reply('there was an error trying to execute that command!');
-    }
-
-
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message
+				.reply(
+					`please wait ${timeLeft.toFixed(
+						1
+					)} more second(s) before reusing the \`${command.name}\` command.`
+				)
+				.then((msg) => {
+					setTimeout(function () {
+						try {
+							msg.delete();
+						} catch (error) {}
+					}, 5000);
+				});
+		}
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+	try {
+		if (command.guildOnly && message.channel.type !== "GUILD_TEXT") {
+			return message.reply(
+				"I can't execute that command inside DMs or threads!"
+			);
+		}
+		if (command.args && !args.length) {
+			const commandhelp = client.commands.get("help");
+			const argshelp = [command.name];
+			commandhelp.execute(message, argshelp);
+		} else {
+			if (command.needsmod) {
+				let isMod = false;
+				modRoles.forEach((element) => {
+					if (
+						currentMember.roles.cache.has(element) ||
+						currentMember.permissions.has(["ADMINISTRATOR"])
+					) {
+						isMod = true;
+					}
+					if (isMod) return;
+				});
+				if (!isMod) {
+					return;
+				} else {
+					exeCommand(command, message, args);
+				}
+			} else if (command.needsadmin) {
+				if (
+					currentMember.permissions.has(["ADMINISTRATOR"]) ||
+					message.author.id == ownerID
+				) {
+					exeCommand(command, message, args);
+					return;
+				} else {
+				}
+			} else {
+				exeCommand(command, message, args);
+			}
+		}
+	} catch (error) {
+		console.error(`Command perms check: ${error}`);
+		message.reply("there was an error trying to execute that command!");
+	}
 });
